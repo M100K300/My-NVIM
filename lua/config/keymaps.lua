550a9ce281fe1e1e8da557e31914c61c4d5d54be
m100k300 <96502613+M100K300@users.noreply.github.com>
2024-12-01 20:37:54 +0000

    changed d to delete without yanking. enabled search highlighting by default
    * changed d to yank to the empty registery
    * Search highlighting works on startup now

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,19 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Remap 'x' to delete without copying
+vim.api.nvim_set_keymap("n", "x", '"_x', { noremap = true, silent = true })
+
+-- Remap 'd' to delete without copying to clipboard
+vim.api.nvim_set_keymap("n", "d", '"_d', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "D", '"_D', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("v", "d", '"_d', { noremap = true, silent = true })
+
+-- Map <leader>d to delete and copy to clipboard
+vim.api.nvim_set_keymap("n", "<leader>d", '""d', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>D", '""D', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("v", "<leader>d", '""d', { noremap = true, silent = true })
+
+-- Turn Syntax Highlighting On (equivalent to set hlsearch in Vim)
+vim.o.hlsearch = true
